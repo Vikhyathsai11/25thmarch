50a170d14a90d1d2db4374cd1b30454174ba95c3
vikhyath <vikhyathsai11@gmail.com>
2023-03-25 14:53:19 +0530

    1stt commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calc';
+  title = 'calci';
+  value:any
+  currentValue=''
+  writeToInput(num:any){
+    this.currentValue=this.currentValue+num
+    console.log(this.currentValue);
+    this.value=this.currentValue;
+
+
+  }
+  back(){
+    this.currentValue =this.currentValue.slice(0,-1);
+    this.value=this.currentValue
+    if(this.value == '')
+    this.value='0'
+    this.currentValue=this.value
+
+  }
+
+  clearInput(){
+      this.value=''
+      this.currentValue=this.value
+
+  }
+  evaluateExp(){
+    this.value=eval(this.currentValue)
+    this.currentValue=this.value
+  }
 }
